53a48b656bc75c9a1527453945a1b009f1e3790c
Diego Magela <diegom@marketpsychdata.com>
2025-05-13 19:19:25 -0300

    importing class from __all__

--- pandasheets/__init__.py
+++ pandasheets/__init__.py
@@ -1,7 +1,5 @@
-from .pandasheets import get_sheet_to_dataframe, upload_dataframe_to_spreadsheet, get_spreadsheet, append_dataframe_to_sheet
+from .pandasheets import pandasheets
 
 __all__ = [
-    'get_sheet_to_dataframe', 
-    'upload_dataframe_to_spreadsheet',
-    'append_dataframe_to_sheet'
+    'pandasheets',
 ]
